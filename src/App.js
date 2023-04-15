d0296d0fd4a1e76272155067a1383ca5a1d9e3fa
milliorn <scottmilliorn@gmail.com>
2023-04-15 16:25:14 -0700

    Preview

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import { useState, useEffect } from "react";
 import { marked } from "marked";
 import { Header } from "./components/Header";
+import { Preview } from "./components/Preview";
 import { TextArea } from "./components/TextArea";
 
 function App() {
@@ -46,15 +47,7 @@ function example() {
 
         <div className="flex flex-col items-center justify-center min-h-full">
           <TextArea text={text} handleChange={handleChange} />
-
-          <h2 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-medium mb-0 bg-sky-100 w-72 sm:w-10/12 text-center mt-4 rounded-t-md max-w-7xl">
-            Preview:
-          </h2>
-
-          <div
-            id="preview"
-            className="w-72 sm:w-10/12 h-64 max-w-7xl p-2 mt-0 border-2 border-sky-400 shadow-md overflow-y-auto bg-sky-50 rounded-b-md"
-          ></div>
+          <Preview />
         </div>
       </div>
 
@@ -77,3 +70,5 @@ function example() {
 }
 
 export default App;
+
+
---
+++ src/components/Preview.js
@@ -0,0 +1,13 @@
+export function Preview() {
+  return (
+    <>
+      <h2 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-medium mb-0 bg-sky-100 w-72 sm:w-10/12 text-center mt-4 rounded-t-md max-w-7xl">
+        Preview:
+      </h2>
+      <div
+        id="preview"
+        className="w-72 sm:w-10/12 h-64 max-w-7xl p-2 mt-0 border-2 border-sky-400 shadow-md overflow-y-auto bg-sky-50 rounded-b-md"
+      ></div>
+    </>
+  );
+}
